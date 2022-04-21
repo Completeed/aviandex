2b2850bac5c9de329b8a96653daa3993551f5be7
primboi <81822069+primitt@users.noreply.github.com>
2022-04-21 11:15:16 -0700

    Add more imports and initializers

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 # imports
 from flask import Flask, url_for, render_template, redirect # more imports can be added 
 from bitcoin import * # ask peer about these imports
-from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+# from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 
 # initializers
 app = Flask(__name__)
-rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%("username", "password")) # change username and password
+# def connect_to_bitcoind_rpc(self):
+#         for i in range(1,settings.rcp_reconnect_max_retry+1):
+#             try:
+#                 self.proxy = bitcoin.rpc.Proxy()
+#                 return
+#             except http.client.HTTPException:
+#                 print("Caught a connection error from Bitcoind RCP, Reconnecting...(%d/%d)" %(i,settings.rcp_reconnect_max_retry)) 
+# Peer can set this up, not sure how to
 
 @app.route('/')
 def index():
